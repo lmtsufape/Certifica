b4910653305f824b822c137904507e945e9ff66e
Douglas Batista <batista.douglasfs@gmail.com>
2022-10-25 16:13:47 -0300

    Alteracoes na view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,6 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <x-jet-banner />
-
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
 
