69b8b9488a3ff355f1bb9f9bbe401830b7796d3e
Douglas Batista <batista.douglasfs@gmail.com>
2022-10-25 11:43:48 -0300

    Migration e model Atividade

---
+++ app/Models/Atividade.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Atividade extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'atividades';
+    
+    protected $fillable = ['status', 'descricao', 'info', 'data_inicio', 'data_fim', 'carga_horaria', 'acao_id'];
+
+}
