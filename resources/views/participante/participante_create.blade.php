f47b374825ae7b7ef905b8b4ffa8127670501a26
Douglas Batista <batista.douglasfs@gmail.com>
2022-11-18 13:15:59 -0300

    Autenticação usando bootstrap

---
+++ resources/views/participante/participante_create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title') Cadastrar Participantes @endsection
+
+@section('content')
+
+  <button type="button" class="btn btn-primary">Primary</button>
+
+@endsection
