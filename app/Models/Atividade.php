30059208c73bc66e78957d72c056e3f056c121be
Douglas Batista <batista.douglasfs@gmail.com>
2022-11-22 18:21:25 -0300

    Inserção do HasFactory em alguns models para finalizar os Factories

--- app/Models/Atividade.php
+++ app/Models/Atividade.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Atividade extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     protected $table = 'atividades';
-    
+
     protected $fillable = ['status', 'descricao', 'info', 'data_inicio', 'data_fim', 'carga_horaria', 'acao_id'];
 
 }
--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Certificado extends Model
 {
-    
+    use HasFactory;
 }
