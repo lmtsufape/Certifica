b0896bf839a44d75ae9ed4795da5795c38404e5e
WhiteHabbyt <netobatista090@gmail.com>
2023-02-09 19:10:58 -0300

    Home Page complite

---
+++ resources/views/layouts/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="container-fluid">
+    <div>
+        <img width="130px" src="{{asset('images/logo-ufape.png')}}">
+    </div>
+    <div>
+        <img width="130px" src="{{asset('images/logo-lmts.png')}}">
+    </div>
+</footer>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset ('css/main/style.css') }}">
+    <link rel="stylesheet" href="{{asset ('css/main/util.css') }}">
+</head>
+<body>
+    <section>
+        @include('layouts.components.barra-brasil')
+        @yield('header')
+    </section>
+
+    <section>
+        @yield('content')
+    </section>
+
+    <section>
+        @include('layouts.components.footer')
+    </section>
+
+</body>
+</html>
