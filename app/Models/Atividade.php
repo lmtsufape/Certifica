1a0dfea40eacc8e45d92daa14e65f451902c5aca
Douglas Filipe <batista.douglasfs@gmail.com>
2023-03-02 13:09:46 -0300

    Atualização nos models das entidades Atividade e Participante

--- app/Models/Atividade.php
+++ app/Models/Atividade.php
@@ -15,20 +15,16 @@ class Atividade extends Model
     protected $fillable = [
         'status',
         'descricao',
-        'info',
         'data_inicio',
         'data_fim',
-        'carga_horaria',
         'acao_id'
     ];
 
     public static $rules = [
         'status' => 'required',
         'descricao' => 'required|min:5',
-        'info' => 'required|min:10',
         'data_inicio' => 'required|date',
         'data_fim' => 'required|date',
-        'carga_horaria' => 'required',
         'acao_id' => 'required',
     ];
 
