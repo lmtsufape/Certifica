0d1a885f0cc371a6d6e9cc39cad6ff09374c4fe9
lucas messias <lmelo072002@gmail.com>
2023-03-17 16:32:59 -0300

    Correção Home-page-front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.components.geral')
 
 
-@section('title','Home Page')
+@section('content-geral')
+    <section class="home-page-home">
 
+        <div class="container d-flex align-items-center row">
 
-@section('content')
+            <div class=" col-6 home-page-welcome-text text-center">
+                <div class="row align-items-center ">
+                <p  >Lorem ipsum ahuisn  <img
+                    src="{{asset('images/home-page/logo-certifica.png')}}"
+                    width= "150"
+                    alt="">
+                </p>
 
-<section class="home-page-home">
+                <p>sed do eiusmod tempor.</p>
+                </div>
+                <img src="{{asset('images/home-page/pessoas-home-page.png')}}" alt="">
 
-    <div class="container d-flex align-items-center row">
-
-        <div class=" col-6 home-page-welcome-text text-center">
-            <div class="row align-items-center ">
-            <p  >Lorem ipsum ahuisn  <img
-                src="{{asset('images/home-page/logo-certifica.png')}}"
-                width= "150"
-                alt="">
-            </p>
-
-            <p>sed do eiusmod tempor.</p>
             </div>
-            <img src="{{asset('images/home-page/pessoas-home-page.png')}}" alt="">
 
+            <div class="col-6">
+                @include('layouts.components.home-page-form')
+            </div>
         </div>
 
-        <div class="col-6">
-            @include('layouts.components.home-page-form')
-        </div>
-    </div>
-
-</section>
-
-
-
+    </section>
 @endsection
+
+
