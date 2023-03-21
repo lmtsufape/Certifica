098e9d084cd6ab1fca512017b5415e8b742c1a04
Douglas Filipe <batista.douglasfs@gmail.com>
2023-03-21 14:07:51 -0300

    Adicionando a rota correta no link de criação de conta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,34 +12,34 @@
         <form class="form-homepage" method="POST" action="{{route('login') }}">
             @csrf
             <h4 class="text-center">Titulo</h4>
-            
-            <input 
-                class="input-home-form" 
-                type="email" 
-                name="email" 
+
+            <input
+                class="input-home-form"
+                type="email"
+                name="email"
                 placeholder="Insira seu e-mail"
                 autofocus
             id="">
-            
-            <input 
-                class="input-home-form" 
-                type="password" 
-                name="password" 
+
+            <input
+                class="input-home-form"
+                type="password"
+                name="password"
                 placeholder="Digite sua senha"
                 autofocus
             id="">
             <div class="container-text-homeform">
                 <a class="esqueceu-senha-link" href="{{ route('password.request') }}">
-                    <p class="text-end text-homepage">Esqueceu sua senha ?</p> 
+                    <p class="text-end text-homepage">Esqueceu sua senha ?</p>
                 </a>
             </div>
             <div>
                 <button class="button-homepage" type="submit">Entrar</button>
             </div>
             <div class="container-text-homeform">
-                <p class="text-homepage">Não possui conta ? <a class="criar-conta-link" href="">Criar Conta</a></p> 
+                <p class="text-homepage">Não possui conta? <a class="criar-conta-link" href="{{ route('register') }}">Criar Conta</a></p>
             </div>
-        </form> 
+        </form>
     </section>
     </html>
 @endsection
--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,21 +1,23 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-white">
     <ul class="navbar-nav ms-auto mr-4">
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle text-black" href="#" role="button"
-               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v- style="color: white">
-                <span class="font-weight-bolder">Olá, </span>{{ Auth::user()->name }}
-            </a>
-
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}">
-                    {{ __('Sair') }}
+        @if(Auth::check())
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle text-black" href="#" role="button"
+                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v- style="color: white">
+                    <span class="font-weight-bolder">Olá, </span>{{ Auth::user()->name }}
                 </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </li>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}">
+                        {{ __('Sair') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endif
     </ul>
 </nav>
 
