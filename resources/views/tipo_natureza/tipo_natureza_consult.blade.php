5d843c5a5616829cf82c85da2d1f1c6b001af340
Douglas Filipe <batista.douglasfs@gmail.com>
2023-03-24 00:32:42 -0300

    Atualização nas views do CRUD Tipo Natureza

--- resources/views/tipo_natureza/tipo_natureza_consult.blade.php
+++ resources/views/tipo_natureza/tipo_natureza_consult.blade.php
@@ -1,64 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class='container'>
-    <div class="row" >
-        @if(session('mensagem'))
-        <div class="col-md-12" style="margin-top: 30px;">
-            <div class="alert alert-success">
-                <p>{{session('mensagem')}}</p>
+    <div class="container">
+        <div class="row" >
+            @if(session('mensagem'))
+            <div class="col-md-12" style="margin-top: 30px;">
+                <div class="alert alert-success">
+                    <p>{{session('mensagem')}}</p>
+                </div>
+            </div>
+            @endif
+        </div>
+
+        <div class="row" >
+            @if(session('error_mensage'))
+            <div class="col-md-12" style="margin-top: 30px;">
+                <div class="alert alert-danger">
+                    <p>{{session('error_mensage')}}</p>
+                </div>
+            </div>
+            @endif
+        </div>
+
+        <div class="text-center" style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+            <h2>Tipos de Natureza</h2>
+        </div>
+
+        <div class='row justify-content-end' style="padding-bottom: 5px; margin-bottom: 10px">
+            <div class='col col-1'>
+                <a href="{{route('tipo_natureza.create')}}" class="btn btn-success">Cadastrar</a>
             </div>
         </div>
-        @endif
-    </div>
 
-    <div class="row" >
-        @if(session('error_mensage'))
-        <div class="col-md-12" style="margin-top: 30px;">
-            <div class="alert alert-danger">
-                <p>{{session('error_mensage')}}</p>
-            </div>
-        </div>
-        @endif
-    </div>
-
-    <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
-        <h2>CONSULTAR TIPOS DE NATUREZA</h2>
-    </div>
-    <div class='row justify-content-end' style="padding-bottom: 5px; margin-bottom: 10px">
-        <div class='col col-1'>
-            <a href="{{route('tipo_natureza.create')}}" class="btn btn-success">Cadastrar</a>
-        </div>
-    </div>
-    
-    <div >
-        <table id="tableMateriais" class="table table-hover table-responsive-md">
+        <table class="table table-hover table-responsive-md">
             <thead style="background-color: #151631; color: white; border-radius: 15px">
                 <tr>
-                    <th>ID</th>
-                    <th>Descrição</th>
-                    <th></th>
+                    <th scope="col"></th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col"></th>
                 </tr>
             </thead>
+
             <tbody>
-                @foreach($tipo_naturezas as $natureza)
+            @foreach($tipo_naturezas as $natureza)
                 <tr>
-                    <td class='col-sm-1'>{{$natureza->id}}</td>
-                    <td class='col-sm-9'>{{$natureza->descricao}}</td>
-                    <td class='col-sm-2'>
-                        <div class="row justify-content-end">
-                            <div class='col-4'>
-                                <a class="btn btn-success" href ="{{ route('tipo_natureza.edit', ['id' => $natureza->id]) }}">Editar</a>
-                            </div>
-                            <div class='col-5'>
-                                <a class="btn btn-danger" href ="{{ route('tipo_natureza.delete', ['id' => $natureza->id]) }}">Apagar</a>
-                            </div>
-                        </div>
+                    <td></td>
+                    <td>{{$natureza->descricao}}</td>
+                    <td class="text-center">
+                        <a class="btn btn-secondary" href="{{ route('tipo_natureza.edit', ['id' => $natureza->id]) }}">Editar</a>
+
+                        <a class="btn btn-danger" href="{{ route('tipo_natureza.delete', ['id' => $natureza->id]) }}">Apagar</a>
                     </td>
                 </tr>
-                @endforeach
+            @endforeach
             </tbody>
         </table>
     </div>
-</div>
 @endsection
--- resources/views/tipo_natureza/tipo_natureza_edit.blade.php
+++ resources/views/tipo_natureza/tipo_natureza_edit.blade.php
@@ -1,36 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class='container'>
-    <div class="row" >
-        @if($errors->any())
-        <div class="col-md-12" style="margin-top: 30px;">
-            <div class="alert alert-danger">
-                @foreach ($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </div>
-        </div>
-        @endif
-    </div>
-
-</div>
-<form action="{{Route('tipo_natureza.update', ['id' => $tipo_natureza->id])}}" method="POST" enctype="multipart/form-data" >
-    @csrf
-    @method('put') 
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <div class="form-row">
-                    <div class="form-group">
-                        <label for="descricao">Descrição</label>
-                        <input name="descricao" type="text" class="form-control" id="descricao" value={{$tipo_natureza->descricao}}>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Editar</button>
+    <div class="container">
+        <div class="row" >
+            @if($errors->any())
+            <div class="col-md-12" style="margin-top: 30px;">
+                <div class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
                 </div>
             </div>
-            <div class="col-md-3"></div>
+            @endif
         </div>
-    </form>
+
+        <div class="text-center" style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+            <h2>Editar Tipo de Natureza</h2>
+        </div>
+
+        <form action="{{Route('tipo_natureza.update', ['id' => $tipo_natureza->id])}}" method="POST" enctype="multipart/form-data" >
+            @csrf
+            @method('put')
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <div class="form-row">
+                            <div class="form-group">
+                                <label for="descricao">Descrição</label>
+                                <input name="descricao" type="text" class="form-control" id="descricao" value="{{$tipo_natureza->descricao}}">
+                            </div>
+
+                            <button type="submit" class="btn btn-success">Atualizar</button>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
+            </form>
+    </div>
 @endsection
