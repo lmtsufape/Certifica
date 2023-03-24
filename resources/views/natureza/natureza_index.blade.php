8f28480fffceb0044236f00027cc1caeca56c43b
Douglas Filipe <batista.douglasfs@gmail.com>
2023-03-24 00:43:54 -0300

    Atualização nas views do CRUD Natureza

--- resources/views/natureza/natureza_index.blade.php
+++ resources/views/natureza/natureza_index.blade.php
@@ -5,53 +5,48 @@
 @endsection
 
 @section('content')
-<div class='container'>
-    <div class="row" >
-        @if(session('mensagem'))
-        <div class="col-md-12" style="margin-top: 30px;">
-            <div class="alert alert-success">
-                <p>{{session('mensagem')}}</p>
+    <div class="container">
+        <div class="row" >
+            @if(session('mensagem'))
+            <div class="col-md-12" style="margin-top: 30px;">
+                <div class="alert alert-success">
+                    <p>{{session('mensagem')}}</p>
+                </div>
+            </div>
+            @endif
+        </div>
+
+        <div class="text-center" style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
+            <h2>Naturezas</h2>
+        </div>
+        <div class='row justify-content-end' style="padding-bottom: 5px; margin-bottom: 10px">
+            <div class='col col-1'>
+                <a href="{{route('natureza.create')}}" class="btn btn-success">Cadastrar</a>
             </div>
         </div>
-        @endif
-    </div>
 
-    <div style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
-        <h2>Naturezas <a class="btn btn-primary" href="{{ route('natureza.create') }}"
-                             role="button">Cadastrar</a> </h2>
-
-    </div>
-
-    <table class="table">
-        <thead class="thead-dark">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Tipo Natureza</th>
-            <th scope="col">Unidade Administrativa</th>
-        </tr>
-        </thead>
-
-        <tbody>
-        @foreach($naturezas as $natureza)
+        <table class="table table-hover table-responsive-md">
+            <thead style="background-color: #151631; color: white; border-radius: 15px">
             <tr>
-                <td></td>
-                <td>{{ $natureza->descricao }}</td>
-                <td>{{ $natureza->tipo_natureza_id }}</td>
-                <td>{{ $natureza->unidade_administrativa_id }}</td>
-                <td>
-                    <div class="dropdown">
-                        <div>
-                            <a class="dropdown-item" href ="{{ route('natureza.edit', ['natureza_id' => $natureza->id]) }}">Editar</a>
-                        </div>
-                        <div>
-                            <a class="dropdown-item" href ="{{ route('natureza.delete', ['natureza_id' => $natureza->id]) }}">Apagar</a>
-                        </div>
-                    </div>
-                </td>
+                <th scope="col"></th>
+                <th scope="col">Descrição</th>
+                <th scope="col"></th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+
+            <tbody>
+            @foreach($naturezas as $natureza)
+                <tr>
+                    <td></td>
+                    <td>{{ $natureza->descricao }}</td>
+                    <td>
+                        <a class="btn btn-secondary" href ="{{ route('natureza.edit', ['natureza_id' => $natureza->id]) }}">Editar</a>
+
+                        <a class="btn btn-danger" href ="{{ route('natureza.delete', ['natureza_id' => $natureza->id]) }}">Apagar</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
