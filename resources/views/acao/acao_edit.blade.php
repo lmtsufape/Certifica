ed30e51caadbc99361db4d6d871078e4e47d8352
Lucas Melo <lmelo072002@gmail.com>
2023-04-27 16:13:35 -0300

    Correcao view editar/criar acao

--- resources/views/acao/acao_edit.blade.php
+++ resources/views/acao/acao_edit.blade.php
@@ -31,50 +31,48 @@
         <input type="hidden" name="unidade_administrativa_id" value="{{ Auth::user()->unidade_administrativa_id }}">
 
         <div class="row d-flex aligm-items-start justify-content-start ">
-            <div class="col-5 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+            <div class="col-md-5 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
                 <span class="tittle-input ">Título</span><input value="{{ $acao->titulo }}" class="w-75 input-text "
                     type="text" name="titulo" id="">
             </div>
-            <div class="col-3 spacing-row1 input-create-box">
-                <span class="tittle-input w-50">Data de inicio</span><input value="{{ $acao->data_inicio }}" class="w-75"
+            <div class="col-md-3 spacing-row1 input-create-box">
+                <span class="tittle-input w-100">Data de inicio</span><input value="{{ $acao->data_inicio }}" class="w-100"
                     type="date" name="data_inicio" id="">
             </div>
-            <div class="col-3 input-create-box">
-                <span class="tittle-input w-50">Data de fim</span><input value="{{ $acao->data_fim }}" class="w-75"
+            <div class="col-md-3 input-create-box">
+                <span class="tittle-input w-100">Data de fim</span><input value="{{ $acao->data_fim }}" class="w-100"
                     type="date" name="data_fim" id="">
             </div>
         </div>
 
-        <div class="row d-flex justify-content-start align-items-start">
-            <div class="spacing-row2 col-4 input-create-box d-flex aligm-items-start justify-content-start flex-column">
+
+
+         <div class="row d-flex aligm-items-start justify-content-start">
+
+            <div class="spacing-row1 col-md-5 input-create-box d-flex aligm-items-start justify-content-start flex-column">
                 <span class="tittle-input w-25">Natureza</span>
-
-                <select name="natureza_id" class="select-form w-75 ">
-
-                    <option value=" {{ $acao->natureza_id }}" selected hidden>{{ $natureza->descricao }}</option>
+                <select class="select-form w-100 " name="natureza_id" id="">
+                    <option value="{{$natureza->descricao}}">{{$natureza->descricao}}</option>
                     @foreach ($naturezas as $natureza)
                         <option value="{{ $natureza->id }}">{{ $natureza->descricao }}</option>
                     @endforeach
-
                 </select>
-
             </div>
-            <div class="col-2 input-create-box input-create-box d-flex aligm-items-start justify-content-start flex-column">
+
+            <div
+                class="col-md-3 spacing-row1 input-create-box input-create-box d-flex aligm-items-start justify-content-start flex-column">
                 <span class="tittle-input n-edital">Nº edital</span><input class="w-50 input-text" type="text"
                     name="" id="">
             </div>
-            <div class="col-5 input-create-box border-0 d-flex flex-column justify-content-between">
-                <input type="file" name="anexo" id="anexo" placeholder="a">
-                <label class="label-file input-create-box d-flex aligm-items-start justify-content-center" for="anexo"
-                    id="">arquivo.pdf(estatic)</label>
 
-            </div>
-        </div>
+            <input hidden type="file" name="anexo" id="anexo" placeholder="a">
 
-        <div class="row d-flex justify-content-start align-items-center">
-            <div class="boxicon col-11 d-flex align-items-center justify-content-end ">
+            <div class="col-md-3 label-file border input-create-box d-flex align-items-center justify-content-center">
 
-                <label for="anexo" id=""><img src="/images/acoes/create/upload.svg" alt=""></label>
+                <label class="w-100 d-flex align-items-center justify-content-beetwen" for="anexo" id="">
+                    <span class="Nome-arquivo">arquivo.pdf(estatic)</span> 
+                    <img class="upload-icon" src="/images/acoes/create/upload.svg" alt="">
+                </label>
 
             </div>
         </div>
@@ -82,11 +80,10 @@
         <div class="row d-flex justify-content-start align-items-center">
             <div class="col d-flex justify-content-evenly align-items-center input-create-box border-0">
                 <a class="d-flex justify-content-center align-items-center cancel" href={{ route('home') }}>Cancelar</a>
-                <button class="submit" type="submit">Atualizar</button>
+                <button class="submit" type="submit">Cadastrar</button>
             </div>
         </div>
 
     </form>
-
 
 @endsection
