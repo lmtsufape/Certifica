d0a002b9b37f4b68af6a1990c61a3f56140f80a3
Lucas Melo <lmelo072002@gmail.com>
2023-04-26 22:15:43 -0300

    View Criar atividade - Coordenador

--- resources/views/atividade/atividade_create.blade.php
+++ resources/views/atividade/atividade_create.blade.php
@@ -4,58 +4,68 @@
     Cadastrar Atividades
 @endsection
 
-@section('content')
-<div class="container">
-
-    @if($errors->any()))
-    <div class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-        @endforeach
-    </div>
-    @endif
-
-    
-    <form action="{{Route('atividade.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        
-        <input type="hidden" name="acao_id" value="{{ $acao->id }}">
-        
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <div class="form-row">
-                    <div class="form-group">
-                        <label for="descricao_atividade">Descricao</label>
-                        <select name="descricao" id="descricao_atividade" class="form-control">
-                            <option value="" selected hidden>Escolher...</option>
-                            @foreach($descricoes as $descricao)
-                                <option value="{{ $descricao }}">{{ $descricao }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="inicio_atividade">Início</label>
-                        <input name="data_inicio" type="date" class="form-control" id="inicio_atividade" >
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="fim_atividade">Fim</label>
-                        <input name="data_fim" type="date" class="form-control" id=fim_atividade" >
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="atividade_acao">Ação</label>
-                        <input name="titulo" type="text" class="form-control" id="acao_titulo" value="{{ $acao->titulo }}" disabled>
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary">Cadastrar</button>
-                </div>
-            </div>
-            <div class="col-md-3"></div>
-        </div>
-    </form>
-</div>
+@section('css')
+    <link rel="stylesheet" href="/css/acoes/create.css">
 @endsection
-    
+
+@section('content')
+    <div class="container">
+
+        @if ($errors->any())
+            )
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </div>
+        @endif
+    </div>
+
+    <h1 class="text-center">Cadastrar atividade</h1>
+    <form class="container form" action="{{ Route('atividade.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        <input type="hidden" name="acao_id" value="{{ $acao->id }}">
+
+        <div class="row d-flex aligm-items-start justify-content-start ">
+
+            <div class="col-7 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+                <span class="tittle-input">Atividade</span>
+                <select class="select-form w-75 " name="descricao" id="">
+                    <option value="" selected hidden>Escolher...</option>
+                    @foreach ($descricoes as $descricao)
+                        <option value="{{ $descricao }}">{{ $descricao }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="col-4 spacing-row1 input-create-box">
+                <span class="tittle-input w-50">Data de inicio</span>
+                <input class="w-75" type="date" name="data_inicio" id="">
+            </div>
+
+        </div>
+
+        <div class="row d-flex aligm-items-start justify-content-start">
+
+            <div class="col-7 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+                <span class="tittle-input">Ação</span>
+                <input value="{{$acao->titulo}}" class="w-75 input-text" type="text" name="titulo" id="" disabled>
+            </div>
+
+            <div class="col-4 input-create-box">
+                <span class="tittle-input w-50">Data de fim</span>
+                <input class="w-75" type="date" name="data_fim" id="">
+            </div>
+
+        </div>
+
+        <div class="row d-flex justify-content-start align-items-center">
+            <div class="col d-flex justify-content-evenly align-items-center input-create-box border-0">
+                <a class="d-flex justify-content-center align-items-center cancel" href={{ Route('atividade.index',['acao_id' => $acao->id ])}}> Cancelar</a>
+                <button class="submit" type="submit">Cadastrar</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
