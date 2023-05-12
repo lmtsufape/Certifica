82bf111c068ed47a2282a9432cdee272d9c52166
Lucas Messias <lmelo072002@gmail.com>
2023-05-12 08:58:45 -0300

    Correcao de responsividade home + footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,32 +1,32 @@
 <footer class="container-fluid pt-1 mt-5" >
 
-    <div class="container-sm px-lg-5">
+    <div class="container px-lg-5">
 
         <div class="row justify-content-between  my-2">
 
-            <div class="col-sm-4 text-center d-flex flex-column align-items-center justify-content-end py-1 border-right">
+            <div class="col-4 text-center d-flex flex-column align-items-center justify-content-end py-1 border-right">
                 <div class="col-sm-8 d-flex h-100 align-items-end justify-content-end py-1">
                     <a class="navbar-brand " href="{{ Route('home') }}">
-                        <img class="logo-certifica" src="{{url('/images/layouts/footer/logo-branca.svg')}}" alt="">
+                        <img class="logo-certifica-footer" src="{{url('/images/layouts/footer/logo-branca.svg')}}" alt="">
                     </a>
                 </div>
                 
             </div>
 
-            <div class="col-sm-4 text-center d-flex flex-column align-items-center justify-content-evenly py-1 border-right">
+            <div class="col-4 text-center d-flex flex-column align-items-center justify-content-evenly py-1 border-right">
 
                 <span class="text-white mb-3">Desenvolvido por</span>
                     
                 <div class="w-100 d-flex flex-row align-items-center justify-content-around">
 
-                    <div class="col-sm-5 d-flex align-items-center justify-content-end">
+                    <div class="col-5 d-flex align-items-center justify-content-end">
                         <a href="http://ufape.edu.br/" target="_blank">
 
                             <img class="logos" src="/images/layouts/footer/UFAPElogo.svg" alt="Logo">
                         </a>
                     </div>
 
-                    <div class="col-sm-5 d-flex align-items-center justify-content-start">
+                    <div class="col-5 d-flex align-items-center justify-content-start">
                         <a href="http://lmts.uag.ufrpe.br/" target="_blank">
 
                             <img class="logos" src="/images/layouts/footer/LMTSlogo.svg" alt="Logo">
@@ -36,11 +36,11 @@
            
             </div>
 
-            <div class="box-redes col-sm-4 d-flex flex-column align-items-start justify-content-evenly py-1">
+            <div class="box-redes col-4 d-flex flex-column align-items-start justify-content-evenly py-1">
 
                 <span class="text-white text-center w-50">Contato</span>
                     
-                <div class="w-75 col-sm-12 d-flex align-items-center justify-content-evenly">
+                <div class="w-75 col-12 d-flex align-items-center justify-content-evenly">
                     <a href="mailto:lmts@ufape.br" target="_blank" class="text-icons">
                         <img class="logo-redes" src="/images/layouts/footer/email.svg">
                     </a>
