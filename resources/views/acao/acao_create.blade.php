eb7278376a878b2c20e81fa58e5c507bf20b5747
Lucas Messias <lmelo072002@gmail.com>
2023-05-13 09:36:31 -0300

    Correcao view cadastrar acao + JS do input file

--- resources/views/acao/acao_create.blade.php
+++ resources/views/acao/acao_create.blade.php
@@ -8,29 +8,51 @@
     <h1 class="text-center">Cadastrar ação</h1>
     <form class="container form" action="{{ Route('acao.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-
+        <!-- Hiddens-->
         <input type="hidden" name="usuario_id" value="{{ Auth::user()->id }}">
         <input type="hidden" name="unidade_administrativa_id" value="{{ Auth::user()->unidade_administrativa_id }}">
 
+
         <div class="row d-flex aligm-items-start justify-content-start ">
-            <div class="col-md-5 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+            <div class="col-md-12 spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
                 <span class="tittle-input ">Título</span>
                 <input class="w-75 input-text " type="text" name="titulo" id="">
-            </div>
-            
-            <div class="col-md-3 spacing-row1 input-create-box">
-                <span class="tittle-input w-50">Data de início</span><input class="w-100" type="date" name="data_inicio"
-                    id="">
-            </div>
-            <div class="col-md-3 input-create-box">
-                <span class="tittle-input w-50">Data de fim</span><input class="w-100" type="date" name="data_fim"
-                    id="">
             </div>
         </div>
 
         <div class="row d-flex aligm-items-start justify-content-start">
+            <div class="col-md-4 spacing-row2 input-create-box">
+                <span class="tittle-input w-50">Data de início</span><input class="w-100" type="date" name="data_inicio"
+                    id="">
+            </div>
+            <div class="col-md-4 spacing-row2 input-create-box">
+                <span class="tittle-input w-50">Data de fim</span><input class="w-100" type="date" name="data_fim"
+                    id="">
+            </div>
+            
+            <div
+                class="col-md-3 input-create-box input-create-box d-flex aligm-items-start justify-content-start flex-column">
+                <span class="tittle-input n-edital">Nº edital</span><input class="w-50 input-text" type="text"
+                    name="" id="">
+            </div>
+        </div>
 
-            <div class="spacing-row1 col-md-5 input-create-box d-flex aligm-items-start justify-content-start flex-column">
+
+        <div class="row d-flex aligm-items-start justify-content-start ">
+
+            <input hidden type="file" name="anexo" id="anexo">
+
+            <div class="col-md-8 spacing-row1 input-create-box border-upload d-flex align-items-start justify-content-start flex-column">
+                <span class="tittle-input ">Arquivo</span>
+                    <div class="w-100 d-flex align-items-center justify-content-between">
+                        <input class="w-75 input-text " type="text" name="" id="arquivo" disabled value="" placeholder="Insira aqui o seu arquivo">
+                        <label for="anexo" id="">
+                            <img class="upload-icon tittle-input" src="/images/acoes/create/upload.svg" alt="">
+                        <label for="anexo" id="">
+                    </div>
+            </div>
+
+            <div class="col-md-3 input-create-box d-flex aligm-items-start justify-content-start flex-column">
                 <span class="tittle-input w-25">Natureza</span>
                 <select class="select-form w-100 " name="natureza_id" id="">
                     @foreach ($naturezas as $natureza)
@@ -39,22 +61,6 @@
                 </select>
             </div>
 
-            <div
-                class="col-md-3 spacing-row1 input-create-box input-create-box d-flex aligm-items-start justify-content-start flex-column">
-                <span class="tittle-input n-edital">Nº edital</span><input class="w-50 input-text" type="text"
-                    name="" id="">
-            </div>
-
-            <input hidden type="file" name="anexo" id="anexo" placeholder="a">
-
-            <div class="col-md-3 label-file border input-create-box d-flex align-items-center justify-content-center">
-
-                <label class="w-100 d-flex align-items-center justify-content-beetwen" for="anexo" id="">
-                    <span class="Nome-arquivo">arquivo.pdf(estatic)</span> 
-                    <img class="upload-icon" src="/images/acoes/create/upload.svg" alt="">
-                </label>
-
-            </div>
         </div>
 
 
@@ -66,4 +72,29 @@
         </div>
 
     </form>
+
+    <script>
+        var campoanexo = document.getElementById('anexo');
+        var campoArquivo = document.getElementById('arquivo')
+
+        campoanexo.addEventListener('change',(e)=>{
+
+           var string = e.target.value 
+
+           var dados = string.split(/[\\"]/g)   
+            
+           campoArquivo.value = dados[dados.length - 1]
+           
+        })
+       
+    </script>
+    
 @endsection
+
+
+
+
+
+
+
+
