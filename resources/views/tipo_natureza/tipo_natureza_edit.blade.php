4d0a8046c05c60f676e4f7e4a6259ba516caccda
Douglas Filipe <batista.douglasfs@gmail.com>
2023-05-18 21:23:49 -0300

    Correção de bug ao editar e salvar o Tipo da Natureza

--- resources/views/tipo_natureza/tipo_natureza_edit.blade.php
+++ resources/views/tipo_natureza/tipo_natureza_edit.blade.php
@@ -22,6 +22,8 @@
             @csrf
             @method('put')
                 <div class="row">
+                    <input type="hidden" name="id" value="{{ $tipo_natureza->id }}">
+
                     <div class="col-md-3"></div>
                     <div class="col-md-6">
                         <div class="form-row">
