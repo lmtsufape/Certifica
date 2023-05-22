3ca5f09a41b1670ba7f5294313d36c142579a34f
Douglas Filipe <batista.douglasfs@gmail.com>
2023-05-22 10:23:06 -0300

    Correção de consulta na home do coordenador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\acao;
+use App\Models\Acao;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
