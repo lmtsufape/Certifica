fe65c3f4577799643f3865d1c2b6790cdb9533f9
Yuri Resende <yresendeo@gmail.com>
2023-05-23 19:16:41 -0300

    Atualizando mensagens das atividades

--- app/Models/Atividade.php
+++ app/Models/Atividade.php
@@ -27,14 +27,14 @@ class Atividade extends Model
     ];
 
     public static $mensages = [
-        'descricao.*'           => 'A descrição deve possuir pelo menos 5 caracteres',
-        'data_inicio.required'  => 'A data de início é obrigatória',
-        'data_inicio.date'      => 'A data de início deve estar no formato data',
-        'data_inicio.before'    => 'A data de início deve ser anterior a data de fim',
-        'data_fim.required'     => 'A data de fim é obrigatória',
-        'data_fim.date'         => 'A data de fim deve estar no formato data',
-        'data_fim.after'        => 'A data de fim deve ser posterior a data de início',
-        'acao_id'               => 'A atividade deve estar vinculada a uma ação',
+        'descricao.*'                   => 'A descrição deve possuir pelo menos 5 caracteres',
+        'data_inicio.required'          => 'A data de início é obrigatória',
+        'data_inicio.date'              => 'A data de início deve estar no formato data',
+        'data_inicio.before_or_equal'   => 'A data de início deve ser anterior a data de fim',
+        'data_fim.required'             => 'A data de término é obrigatória',
+        'data_fim.date'                 => 'A data de término deve estar no formato data',
+        'data_fim.after_or_equal'       => 'A data de término deve ser posterior a data de início',
+        'acao_id'                       => 'A atividade deve estar vinculada a uma ação',
     ];
 
     public function acao(){
