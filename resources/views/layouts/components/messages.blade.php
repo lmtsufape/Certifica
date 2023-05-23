a0a32ccfac1c9a9aaf619af0ba214b51697594e0
Yuri Resende <yresendeo@gmail.com>
2023-05-23 18:38:07 -0300

    Centralizando mensagens de erro e diminuindo o tamanho dos alerts

--- resources/views/layouts/components/messages.blade.php
+++ resources/views/layouts/components/messages.blade.php
@@ -1,25 +1,25 @@
 <div class="container mt-3 mb-3">
-    <div class="row">
+    <div class="row justify-content-center">
         @if(session('mensagem'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-success alert-dismissible fade show" role="alert" style="width:50%">
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 <span>{{session('mensagem')}}</span>
             </div>
         @endif
     </div>
 
-    <div class="row">
+    <div class="row justify-content-center">
         @if(session('error_mensage'))
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert" style="width:50%">
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 <span>{{session('error_mensage')}}</span>
             </div>
         @endif
     </div>
 
-    <div class="row">
+    <div class="row justify-content-center">
         @if($errors->any())
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert" style="width:50%">
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 <ul class="mb-0">
                     @foreach ($errors->all() as $error)
