e08f6a4f992466173342438a670c08b857cebe68
Yuri Resende <yresendeo@gmail.com>
2023-06-08 10:33:48 -0300

    Função para atualizar os dados do usuário no sistema

--- app/Http/Controllers/Auth/EditProfile.php
+++ app/Http/Controllers/Auth/EditProfile.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Perfil;
 use App\Models\Instituicao;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Validates\DefaultValidator;
+use Illuminate\Validation\ValidationException;
 
 class EditProfile extends Controller
 {
@@ -25,6 +29,40 @@ class EditProfile extends Controller
     }
 
     public function update(Request $request){
-        dd($request->all());
+        $user = Auth::user();
+
+        try {
+            DefaultValidator::validate($request->all(), User::$edit_rules, User::$messages);
+
+            if($user->email != $request->email){
+                DefaultValidator::validate($request->all(), User::$email_rules, User::$messages);
+            }
+
+            if($user->cpf != $request->cpf){
+                DefaultValidator::validate($request->all(), User::$cpf_rules, User::$messages);
+            }
+
+            if($request->password_confirmation != null){
+                DefaultValidator::validate($request->all(), User::$password_rules, User::$messages);
+            }
+
+        } catch (ValidationException $exception) {
+            return redirect(route('perfil.edit'))
+                ->withErrors($exception->validator)->withInput();
+        }
+
+
+        if($request->password_confirmation != null){
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->name    = $request->name;
+        $user->cpf     = $request->cpf;
+        $user->email   = $request->email;
+        $user->celular = $request->celular;
+
+        $user->save();
+
+        return redirect(route('perfil.edit'))->with(['mensagem' => "Dados atualizados com sucesso"]);
     }
 }
