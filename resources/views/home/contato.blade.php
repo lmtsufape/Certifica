0cb334f07464a56ab807a62f579e56e3cafede21
Lucas Melo <lmelo072002@gmail.com>
2023-06-14 17:12:22 -0300

    view de contato / validar certificado / o sistema

--- resources/views/home/contato.blade.php
+++ resources/views/home/contato.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="/css/home/contato.css">
+@endsection
+
 @section('content')
-   <h1>contato VIEW</h1>
+    <div class="container">
+
+        <section class="section-view pb-2 pt-3">
+            <h1 class="titulo-view mb-4 mt-2">Contato</h1>
+
+            <div class="pb-1">
+                <h6 class="mb-3">Laboratório Multidisciplinar de Tecnologias Sociais (LMTS)</h3>
+                    Universidade Federal do Agreste de Pernambuco - UFAPE <br>
+                    Avenida Bom Pastor, s/n.º, Bairro Boa Vista - CEP: 55292-270 - Garanhuns - PE <br>
+                    Laboratório 23 <br>
+                    Email: lmts@ufrpe.br <br>
+            </div>
+        </section>
+    </div>
 @endsection
