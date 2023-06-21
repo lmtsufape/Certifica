e5e5a78ca2a9e32e31068311ed9fbffadfa19b59
Yuri Resende <yresendeo@gmail.com>
2023-06-21 17:34:19 -0300

    Envio de email para integrantes não cadastrados (importação de integrantes via CSV)

---
+++ app/Mail/UsuarioNaoCadastrado.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UsuarioNaoCadastrado extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(private readonly array $data)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Finalização de cadastro',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'Email.usuario_nao_cadastrado',
+            with: [
+                'email'    => $this->data['email'],
+                'password' => $this->data['password'],
+                'imagem'   => public_path().'/images/layouts/header/logo-certifica.png'
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
