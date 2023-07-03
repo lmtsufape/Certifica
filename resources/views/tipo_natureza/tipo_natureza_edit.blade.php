ad2cc5a0246c475030384a64612d7eaa97a31c72
Lucas Melo <lmelo072002@gmail.com>
2023-07-03 11:14:44 -0300

    Views de editar/criar/index Tipo natureza:ADM

--- resources/views/tipo_natureza/tipo_natureza_edit.blade.php
+++ resources/views/tipo_natureza/tipo_natureza_edit.blade.php
@@ -1,41 +1,42 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="/css/home/contato.css">
+    <link rel="stylesheet" href="/css/acoes/create.css">
+@endsection
+
 @section('content')
-    <div class="container">
-        <div class="text-center" style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
-            <h2>Editar Tipo de Natureza</h2>
-        </div>
+    <div class='container'>
+        <section class="section-view pb-3 pt-4">
+            <h2 class="titulo-view mb-4">Editar Tipo de Natureza</h2>
 
-        <form action="{{Route('tipo_natureza.update', ['id' => $tipo_natureza->id])}}" method="POST" enctype="multipart/form-data" >
-            @csrf
-            @method('put')
-                <div class="row">
-                    <input type="hidden" name="id" value="{{ $tipo_natureza->id }}">
+            <form action={{Route('tipo_natureza.update', ['id' => $tipo_natureza->id])}} method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('put')
 
-                    <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <div class="form-row">
-                            <div class="form-group">
-                                <label for="descricao">Descrição</label>
-                                <input name="descricao" type="text" class="form-control" id="descricao" value="{{$tipo_natureza->descricao}}">
-                            </div>
+                <input type="hidden" name="id" value="{{ $tipo_natureza->id }}">
 
-                            <div class="form-group">
-                                <label for="tipo_natureza">Natureza</label>
-
-                                <select name="natureza_id" id="natureza" class="form-control">
-                                    <option value="{{ $natureza->id }}" selected hidden>{{ $natureza->descricao }}</option>
-                                    @foreach($naturezas as $natureza)
-                                        <option value="{{ $natureza->id }}">{{ $natureza->descricao }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <button type="submit" class="btn btn-success">Atualizar</button>
-                        </div>
+                <div class="row d-flex aligm-items-start justify-content-start mb-3">
+                    <div class="col-md-6 input-create-box d-flex aligm-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Descrição <span class="ast">*</span> </span>
+                        <input class="w-75 input-text" type="text" name="descricao" id="descricao" 
+                            placeholder="Descrição" required value="{{$tipo_natureza->descricao}}">
                     </div>
-                    <div class="col-md-3"></div>
                 </div>
+                <div class="row d-flex aligm-items-start justify-content-start mb-3">
+                    <div class="col-md-6 input-create-box d-flex aligm-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Descrição <span class="ast">*</span> </span>
+
+                        <select name="natureza_id" id="tipo_natureza"  class="select-form w-100 " required>
+                            <option value="{{ $natureza->id }}" selected hidden>{{ $natureza->descricao }}</option>
+                            @foreach ($naturezas as $natureza)
+                                <option value="{{ $natureza->id }}">{{ $natureza->descricao }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="">Atualizar</button>
             </form>
+        </section>
     </div>
 @endsection
