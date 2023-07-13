c55af8db39f67f7c48fe2d945fdc355ebdebd041
Douglas <batista.douglasfs@gmail.com>
2023-07-13 19:11:35 -0300

    Alterações no model Certificado para utilização do safe delete

--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Certificado extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table = 'certificados';
     protected $fillable = ['atividade_id', 'certificado_modelo_id',
      'assinatura_esquerda', 'img_fundo', 'texto', 'logo'];
