a9f2db1b477653900a27475f652efa29b7bac08b
Yuri Resende <yresendeo@gmail.com>
2023-07-14 17:23:23 -0300

    Substituindo a tag de negrito para * no lugar de .*

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -101,10 +101,6 @@ class CertificadoController extends Controller
         $tipo_natureza = TipoNatureza::findOrFail($acao->tipo_natureza_id);
         $natureza = Natureza::findOrFail($tipo_natureza->natureza_id);
 
-
-        $data_inicio = Carbon::parse($atividade->data_inicio)->isoFormat('LL');
-        $data_fim = Carbon::parse($atividade->data_fim)->isoFormat('LL');
-
         $data_atual = Carbon::parse(Carbon::now())->isoFormat('LL');
 
         $certificado = Certificado::where('cpf_participante', $participante->user->cpf)->where('atividade_id', $atividade->id)->first();
@@ -113,11 +109,7 @@ class CertificadoController extends Controller
 
         $atividade->descricao = Str::lower($atividade->descricao);
 
-        $antes = array('%participante%', '%acao%', '%nome_atividade%', '%atividade%', '%data_inicio%', '%data_fim%', '%carga_horaria%', '%natureza%', '%tipo_natureza%', '.*', '*.');
-        $depois = array($participante->user->name, $acao->titulo, $participante->titulo, $atividade->descricao, $data_inicio, $data_fim,
-                        $participante->carga_horaria, $natureza->descricao, $tipo_natureza->descricao, '<b>', '</b>');
-
-        $modelo->texto = str_replace($antes, $depois, $modelo->texto);
+        $modelo->texto = $this->convert_text($modelo, $participante, $acao, $atividade, $natureza, $tipo_natureza);
 
         $imagem = Storage::url($modelo->fundo);
 
@@ -256,5 +248,27 @@ class CertificadoController extends Controller
         {
             return view('certificado.validar', ['mensagem' => 'Certificado inválido!']);
         }
+    }
+
+    private function convert_text($modelo, $participante, $acao, $atividade, $natureza, $tipo_natureza){
+        $data_inicio = Carbon::parse($atividade->data_inicio)->isoFormat('LL');
+        $data_fim = Carbon::parse($atividade->data_fim)->isoFormat('LL');
+
+        $pattern = '/\*[\w\%\.\,\_\-\(\)\#\@\!\'\"]+\*/i';
+        $replace = '<b>$0</b>';
+
+
+        $modelo->texto = preg_replace($pattern, $replace, $modelo->texto);
+
+        // dd($modelo->texto);
+
+        $antes = array('%participante%', '%acao%', '%nome_atividade%', '%atividade%', '%data_inicio%', '%data_fim%', '%carga_horaria%', '%natureza%', '%tipo_natureza%', '*');
+        $depois = array($participante->user->name, $acao->titulo, $participante->titulo, $atividade->descricao, $data_inicio, $data_fim,
+                        $participante->carga_horaria, $natureza->descricao, $tipo_natureza->descricao, '');
+
+        
+
+
+        return str_replace($antes, $depois, $modelo->texto);
     }
 }
