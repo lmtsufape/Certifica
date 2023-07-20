bbee458ef1550ad8592edd28b876ec25e1505b69
Lucas Melo <lmelo072002@gmail.com>
2023-07-20 17:53:44 -0300

    View edit modelo de certificado

--- resources/views/certificado_modelo/certificado_modelo_edit.blade.php
+++ resources/views/certificado_modelo/certificado_modelo_edit.blade.php
@@ -1,110 +1,105 @@
-@extends('layouts.app')
+<!--View apresentada em gestor -->
 
+@extends('layouts.app')
 
 @section('css')
     <link rel="stylesheet" href="/css/acoes/create.css">
     <link rel="stylesheet" href="/css/modelo_certificado/modelo_certificado.css">
+    <link rel="stylesheet" href="/css/cadastros/cadastrarAcao.css">
 @endsection
 
 @section('content')
-    <h2 class="text-center">ATUALIZAR MODELO DE CERTIFICADO</h2>
+    <div class="container">
+        <h2 class="text-center">ATUALIZAR MODELO DE CERTIFICADO</h2>
 
-    <form class="container form" action="{{ Route('certificado_modelo.update', ['id' => $modelo->id]) }}" method="POST"
-        enctype="multipart/form-data">
-        @method('PUT')
-        @csrf
-        <!--hiddens -->
+        <form action="{{ Route('certificado_modelo.update', ['id' => $modelo->id]) }}" method="POST"
+            enctype="multipart/form-data">
+            @method('PUT')
+            @csrf
 
-        <input type="hidden" name="unidade_administrativa_id" value="1">
+            <div class="form-box-modelo-certificado form-row">
+                <input type="hidden" name="unidade_administrativa_id" value="1">
 
-        <div class="row justify-content-center">
 
-            <div class="row d-flex aligm-items-start justify-content-start">
-                <div class="col spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                    <span class="tittle-input">Título </span>
 
-                    <input class="w-75 input-text" name="descricao" type="text" class="form-control" id="descricao"
-                        placeholder="Nome do modelo" value="{{ $modelo->descricao }}">
+                <div class="row box col-xl-7">
+                    <div class="campo input-create-box d-flex aligm-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Unidade Administrativa</span>
+
+                        <select class="select-form w-100 " name="unidade_adm" id="unidade_adm" class="form-select">
+                            <option value="" selected></option>
+                            @foreach ($unidades as $unidade)
+                                @if ($unidade->id === $modelo->unidade_administrativa_id)
+                                    <option value='{{ $unidade->id }}' selected>{{ $unidade->descricao }}</option>
+                                @else
+                                    <option value='{{ $unidade->id }}'>{{ $unidade->descricao }}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-            </div>
 
-            <div class="row d-flex aligm-items-start justify-content-start">
-                <div
-                    class="col textarea-box spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                    <span class="tittle-input">Texto</span>
+                <div class="row box col-xl-7">
+                    <div class="campo input-create-box d-flex aligm-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Titulo</span>
+                        <input class="w-100 h-100 input-text" name="descricao" type="text" placeholder="Nome do modelo"
+                            value="{{ $modelo->descricao }}">
+                    </div>
+                </div>
 
-                    <textarea class="w-100 input-text textarea-form" name="texto" type="text"
-                        placeholder="Texto padrão do certificado ...">
+                <div class="row box d-flex flex-column col-xl-7">
+                    <span class="tittle-input w-100">Texto padrão:</span>
+
+                    <textarea name="texto" class="w-100 campo input-create-box text-area-campo" id="texto">
                         {{ $modelo->texto }}
                     </textarea>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-1"></div>
+                <div class="row d-flex align-items-center justify-content-evenly col-xl-7">
 
-            <input hidden type="file" name="verso" id="verso" accept="image/*" value={{$modelo->verso}}>
+                    <input hidden type="file" name="fundo" id="plano_fundo" accept="image/*"
+                        value={{ $modelo->fundo }}>
 
-            <div class="col-5 text-center">
-                <label class="label" for="verso">
-                    <span>Verso</span>
-                    <div class="card-preview">
-                        <strong><span id="card_verso">nome arquivo</span></strong>
-                        <span>(preview em desenvolvimento)</span>
+                    <div class="col-5">
+                        <label class="label" for="plano_fundo">
+                            <span>Fundo: </span>
+                            <div class="card-preview border">
+                                <strong>
+                                    <span id="card_plano_fundo">nome arquivo</span>
+                                </strong>
+                                <span>(preview em desenvolvimento)</span>
+                            </div>
+                        </label>
                     </div>
-                </label>
-            </div>
 
-            <input hidden type="file" name="fundo" id="plano_fundo" accept="image/*" value={{$modelo->fundo}}>
+                    <input hidden type="file" name="verso" id="verso" accept="image/*" value={{ $modelo->verso }}>
 
-            <div class="col-5 text-center">
-                <label class="label" for="plano_fundo">
-                    <span>Plano de fundo</span>
-                    <div class="card-preview">
-                        <strong><span id="card_plano_fundo">nome arquivo</span></strong>
-                        <span>(preview em desenvolvimento)</span>
+                    <div class="col-5">
+                        <label class="label" for="verso">
+                            <span>Verso: </span>
+                            <div class="card-preview border">
+                                <strong><span id="card_verso">nome arquivo</span></strong>
+                                <span>(preview em desenvolvimento)</span>
+                            </div>
+                        </label>
                     </div>
-                </label>
+
+                </div>
+
+
+                <div class="row d-flex justify-content-start align-items-center mt-4 col-xl-7">
+
+                    <div class="col d-flex justify-content-evenly align-items-center input-create-box border-0">
+                        <a class="d-flex justify-content-center align-items-center cancel"
+                            href={{ route('certificado_modelo.show', ['id' => $modelo->id]) }}>Voltar</a>
+                        <button class="submit" type="submit">Salvar</button>
+                    </div>
+                </div>
+
             </div>
-
-            <div class="col-1"></div>
-        </div>
-
-
-        <div class="row d-flex aligm-items-start justify-content-start">
-            <div class="col spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                <span class="tittle-input">Unidade Administrativa</span>
-
-                <select class="select-form w-100 " name="unidade_adm" id="unidade_adm" class="form-select">
-                    <option value="" selected></option>
-                    @foreach ($unidades as $unidade)
-                        @if ($unidade->id === $modelo->unidade_administrativa_id)
-                            <option value='{{ $unidade->id }}' selected>{{ $unidade->descricao }}</option>
-                        @else
-                            <option value='{{ $unidade->id }}'>{{ $unidade->descricao }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="mt-5 row align-items-center justify-content-evenly">
-            <div class="col-2">
-                <a href="{{route('certificado_modelo.show',['id'=>$modelo->id])}}">
-                    <button type="submit" class="voltar">Voltar</button>
-                </a>
-            </div>
-
-            <div class="col-2">
-                <button type="submit" class="cadastrar">Salvar</button>
-            </div>
-
-        </div>
-
-    </form>
-
-    <x-legenda/>
+        </form>
+    </div>
+    <x-legenda />
 
     <script>
         //nome do arquivo de plano de fundo
@@ -135,75 +130,3 @@
         })
     </script>
 @endsection
-
-
-
-<!--
-<div class="container">
-    <div class='row justify-content-center'>
-        <div class='col-12' style="border-bottom: #949494 2px solid; padding-bottom: 5px; margin-bottom: 10px">
-            <h2>ATUALIZAR MODELO DE CERTIFICADO</h2>
-        </div>
-    </div>
-
-    <form action="{{ Route('certificado_modelo.update', ['id' => $modelo->id]) }}" method="POST" enctype="multipart/form-data" >
-        @method('PUT')
-        @csrf
-        <input type="hidden" name="unidade_administrativa_id" value="1">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="row">
-
-                    <div class="form-group">
-                        <label for="nome">Descrição</label>
-                        <input name="descricao" type="text" class="form-control" id="descricao" value="{{ $modelo->descricao }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="frente_certificado">Frente do Certificado</label>
-                        <input name="fundo" type="file" class="form-control" id="fundo" accept="image/*" value='<?= $modelo->fundo ?>'>
-                    </div>
-                    <div class="col-3" style='margin-top: 5px;'>
-                        <a href="{{ route('certificado_modelo.show_img', ['id' => $modelo->id, 'imagem' => 'fundo']) }}"  class='btn btn-sm btn-outline-secondary' target="_blank">Ver imagem</a>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="verso_certificado">Verso do Certificado</label>
-                        <input name="verso" type="file" class="form-control" id="verso" accept="image/*" value='<?= $modelo->verso ?>'>
-                    </div>
-                    <div class="col-3" style='margin-top: 5px;'>
-                        <a href="{{ route('certificado_modelo.show_img', ['id' => $modelo->id, 'imagem' => 'verso']) }}"  class='btn btn-sm btn-outline-secondary' target="_blank">Ver imagem</a>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="texto">Texto padrão:</label>
-                        <textarea name="texto" type="text" class="form-control" id="texto" rows='5'>{{ $modelo->texto }}</textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="unidade_adm">Unidade Administrativa</label>
-                        <select name="unidade_adm" id="unidade_adm" class="form-select">
-                            <option value='' selected></option>
-                            @foreach ($unidades as $unidade)
-@if ($unidade->id === $modelo->unidade_administrativa_id)
-<option value='{{ $unidade->id }}' selected>{{ $unidade->descricao }}</option>
-@else
-<option value='{{ $unidade->id }}'>{{ $unidade->descricao }}</option>
-@endif
-@endforeach
-                        </select>
-                    </div>
-                    <div class="row justify-content-end" style='margin-top: 5px;'>
-                        <div class="col-2">
-                            <button type="submit" class="btn btn-primary">Salvar</button>
-                        </div>
-                        <div class="col-2">
-                            <a href="{{ route('certificado_modelo.show', ['id' => $modelo->id]) }}" class='btn btn-primary' style='margin-left: 10px'>Voltar</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
--->
