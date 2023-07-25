b310134c9be12ef044a44bbdad0d68ed4b92833c
Yuri Resende <yresendeo@gmail.com>
2023-07-25 08:59:52 -0300

    Adicionando a legenda na tela de criação de modelo do admin

--- resources/views/certificado_modelo/certificado_modelo_create.blade.php
+++ resources/views/certificado_modelo/certificado_modelo_create.blade.php
@@ -9,99 +9,103 @@
 @endsection
 
 @section('content')
-    <h2 class="text-center">CADASTRAR MODELO DE CERTIFICADOS</h2>
+<div class="row">
+    <div class="container container-form-modelo">
+        <h2 class="text-center">CADASTRAR MODELO DE CERTIFICADOS</h2>
 
-    <form class="container form" action="{{ Route('certificado_modelo.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <!--hiddens -->
-        <input type="hidden" name="unidade_administrativa_id" value="1">
+        <form class="container form" action="{{ Route('certificado_modelo.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <!--hiddens -->
+            <input type="hidden" name="unidade_administrativa_id" value="1">
 
-        <div class="row justify-content-center">
+            <div class="row justify-content-center">
+
+                <div class="row d-flex aligm-items-start justify-content-start">
+                    <div class="col spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Título </span>
+
+                        <input class="w-75 input-text" name="descricao" type="text" class="form-control" id="descricao"
+                            placeholder="Nome do modelo">
+                    </div>
+                </div>
+
+                <div class="row d-flex aligm-items-start justify-content-start">
+                    <div
+                        class="col textarea-box spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
+                        <span class="tittle-input">Texto</span>
+
+                        <textarea class="w-100 input-text textarea-form" name="texto" type="text"
+                            placeholder="Texto padrão do certificado ...">
+                
+                        </textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-1"></div>
+
+                <input hidden type="file" name="verso" id="verso" accept="image/*">
+
+                <div class="col-5 text-center">
+                    <label class="label" for="verso">
+                        <span>Verso</span>
+                        <div class="card-preview">
+                            <strong><span id="card_verso">nome arquivo</span></strong>
+                            <span>(preview em desenvolvimento)</span>
+                        </div>
+                    </label>
+                </div>
+
+                <input hidden type="file" name="fundo" id="plano_fundo" accept="image/*">
+
+                <div class="col-5 text-center">
+                    <label class="label" for="plano_fundo">
+                        <span>Plano de fundo</span>
+                        <div class="card-preview">
+                            <strong><span id="card_plano_fundo">nome arquivo</span></strong>
+                            <span>(preview em desenvolvimento)</span>
+                        </div>
+                    </label>
+                </div>
+
+                <div class="col-1"></div>
+            </div>
+
 
             <div class="row d-flex aligm-items-start justify-content-start">
                 <div class="col spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                    <span class="tittle-input">Título </span>
+                    <span class="tittle-input">Unidade Administrativa</span>
 
-                    <input class="w-75 input-text" name="descricao" type="text" class="form-control" id="descricao"
-                        placeholder="Nome do modelo">
+                    <select class="select-form w-100 " name="unidade_adm" id="unidade_adm" class="form-select">
+                        <option value="" selected></option>
+                        @foreach ($unidades as $unidade)
+                            <option value={{ $unidade->id }}>{{ $unidade->descricao }}</option>
+                        @endforeach
+                    </select>
                 </div>
             </div>
 
-            <div class="row d-flex aligm-items-start justify-content-start">
-                <div
-                    class="col textarea-box spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                    <span class="tittle-input">Texto</span>
-
-                    <textarea class="w-100 input-text textarea-form" name="texto" type="text"
-                        placeholder="Texto padrão do certificado ...">
-            
-                    </textarea>
+            <div class="mt-5 row align-items-center justify-content-evenly">
+                <div class="col-2">
+                    <a class="button" href="{{ route('certificado_modelo.index') }}">
+                        Cancelar
+                    </a>
                 </div>
+
+                <div class="col-2">
+                    <button type="submit" class="cadastrar">Cadastrar</button>
+                </div>
+
             </div>
+
+        </form>
+
+        <div>
+            <x-legenda/>
         </div>
-
-        <div class="row">
-            <div class="col-1"></div>
-
-            <input hidden type="file" name="verso" id="verso" accept="image/*">
-
-            <div class="col-5 text-center">
-                <label class="label" for="verso">
-                    <span>Verso</span>
-                    <div class="card-preview">
-                        <strong><span id="card_verso">nome arquivo</span></strong>
-                        <span>(preview em desenvolvimento)</span>
-                    </div>
-                </label>
-            </div>
-
-            <input hidden type="file" name="fundo" id="plano_fundo" accept="image/*">
-
-            <div class="col-5 text-center">
-                <label class="label" for="plano_fundo">
-                    <span>Plano de fundo</span>
-                    <div class="card-preview">
-                        <strong><span id="card_plano_fundo">nome arquivo</span></strong>
-                        <span>(preview em desenvolvimento)</span>
-                    </div>
-                </label>
-            </div>
-
-            <div class="col-1"></div>
-        </div>
-
-
-        <div class="row d-flex aligm-items-start justify-content-start">
-            <div class="col spacing-row1 input-create-box d-flex align-items-start justify-content-start flex-column">
-                <span class="tittle-input">Unidade Administrativa</span>
-
-                <select class="select-form w-100 " name="unidade_adm" id="unidade_adm" class="form-select">
-                    <option value="" selected></option>
-                    @foreach ($unidades as $unidade)
-                        <option value={{ $unidade->id }}>{{ $unidade->descricao }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="mt-5 row align-items-center justify-content-evenly">
-            <div class="col-2">
-                <a class="button" href="{{ route('certificado_modelo.index') }}">
-                    Cancelar
-                </a>
-            </div>
-
-            <div class="col-2">
-                <button type="submit" class="cadastrar">Cadastrar</button>
-            </div>
-
-        </div>
-
-    </form>
-
-    <!--
-        <x-legenda/>
-    -->
+    </div>
+ </div>  
 
     <script>
         //nome do arquivo de plano de fundo
