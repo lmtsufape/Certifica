3ffa529244402ac002d1f31f8543efa8b7381cdf
Yuri Resende <yresendeo@gmail.com>
2023-07-25 18:59:13 -0300

    Reestilização dos menus de informações do sistema

--- resources/views/certificado/validar_certificado.blade.php
+++ resources/views/certificado/validar_certificado.blade.php
@@ -7,36 +7,35 @@
 
 @section('content')
     <div class='container'>
-        <div class="row">
-
-            @if ($errors->any())
-                <div class="col-md-12" style="margin-top: 30px;">
-                    <div class="alert alert-danger">
-                        @foreach ($errors->all() as $erro)
-                            <li>{{ $erro }}</li>
-                        @endforeach
-                    </div>
-                </div>
-            @endif
-        </div>
-
-        <section class="section-view pb-3 pt-4">
-            <h2 class="titulo-view mb-4">Validar Certificado</h2>
-
-            <form action="{{ Route('validar_certificado.checar') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-    
-                <div class="row d-flex aligm-items-start justify-content-start mb-3">
-                    <div class="col-md-10 input-create-box d-flex aligm-items-start justify-content-start flex-column">
-                        <span class="tittle-input">Validação<strong style="color: red">*</strong></span>
-                        <input class="w-75 input-text " type="text" name="codigo_validacao" id="codigo_validacao"
-                            placeholder="Código de Validação..." required>
-                    </div>
-                </div>
-    
-                <button type="submit" class="">Validar</button>
-            </form>    
-        </section>
         
+        <div class="col-12">
+            <div class="row justify-content-center">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h2 class="titulo-view mb-4">Validar Certificado</h2>
+                        
+                        <form action="{{ Route('validar_certificado.checar') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="row d-flex aligm-items-start justify-content-start mb-3 div-teste">
+                                <div class="col-md-10 d-flex aligm-items-start justify-content-start flex-column">
+                                    <span class="span-text">Validação<strong style="color: red">*</strong></span>
+                                    <input class="w-75 input-text-align " type="text" name="codigo_validacao" id="codigo_validacao"
+                                    placeholder="Código de Validação..." required>
+                                </div>
+                            </div>
+                            
+                            <div class="row justify-content-center">
+                                <div class='col-3'>
+                                    <button type="submit" class="">Validar</button>
+                                </div>
+                            </div>
+                        </form>    
+                    </div>
+                </div>
+            </div>
+        </div>        
     </div>
-@endsection
+    @endsection
+    
