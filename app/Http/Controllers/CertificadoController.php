c82a4fe77a62b361502f9a632b0f56bd2dd922c5
Yuri Resende <yresendeo@gmail.com>
2023-07-27 17:20:45 -0300

    Correção de erro ao submeter ação

--- app/Http/Controllers/CertificadoController.php
+++ app/Http/Controllers/CertificadoController.php
@@ -52,6 +52,8 @@ class CertificadoController extends Controller
 
         }
 
+        $certificados_emitidos = collect();
+
         foreach($atividades as $atividade)
         {
             $participantes = Participante::all()->where("atividade_id", $atividade->id);
@@ -63,6 +65,8 @@ class CertificadoController extends Controller
                $certificado_modelo =  CertificadoModelo::where("unidade_administrativa_id", Auth::user()->unidade_administrativa_id)->first();
             }
 
+            if(!$certificado_modelo) return redirect()->back()->with(['alert_mensage' => 'É necessário ter pelo menos um modelo de certificado cadastrado para cada atividade da ação!']);
+
             foreach($participantes as $participante)
             {
                 $certificado = new Certificado();
@@ -72,9 +76,11 @@ class CertificadoController extends Controller
                 $certificado->certificado_modelo_id = $certificado_modelo->id;
                 $certificado->atividade_id = $atividade->id;
 
-                $certificado->save();
+                $certificados_emitidos->push($certificado);
             }
         }
+
+        $certificados_emitidos->each(fn($certificado) => $certificado->save());
 
         if (Auth::user()->perfil_id == 3)
         {
