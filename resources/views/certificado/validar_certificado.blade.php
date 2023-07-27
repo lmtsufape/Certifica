b186866a13d35c8f8c9f05237d43d182500ef4b9
Lucas Melo <lmelo072002@gmail.com>
2023-07-26 21:14:10 -0300

    correcao view de autenticidade do certificado + correcao titulo da view tutorial de uso

--- resources/views/certificado/validar_certificado.blade.php
+++ resources/views/certificado/validar_certificado.blade.php
@@ -7,35 +7,39 @@
 
 @section('content')
     <div class='container'>
-        
+
         <div class="col-12">
             <div class="row justify-content-center">
+
                 <div class="card">
                     <div class="card-body">
 
                         <h2 class="titulo-view mb-4">Validar Certificado</h2>
-                        
-                        <form action="{{ Route('validar_certificado.checar') }}" method="POST" enctype="multipart/form-data">
+
+                        <form action="{{ Route('validar_certificado.checar') }}" method="POST"
+                            enctype="multipart/form-data">
                             @csrf
 
-                            <div class="row d-flex aligm-items-start justify-content-start mb-3 div-teste">
-                                <div class="col-md-10 d-flex aligm-items-start justify-content-start flex-column">
-                                    <span class="span-text">Validação<strong style="color: red">*</strong></span>
-                                    <input class="w-75 input-text-align " type="text" name="codigo_validacao" id="codigo_validacao"
-                                    placeholder="Código de Validação..." required>
+                            <div class="row box div-box-input">
+                                <div
+                                    class="col-xl-12 campo input-create-box d-flex aligm-items-start justify-content-start flex-column">
+
+                                    <span style="background: transparent" class="tittle-input">Validação<span
+                                            class="ast">*</span></span>
+                                    <input class="w-100 h-100 input-text" type="text" name="codigo_validacao"
+                                        id="codigo_validacao" placeholder="Código de Validação..." required>
                                 </div>
                             </div>
-                            
+
+
+
                             <div class="row justify-content-center">
-                                <div class='col-3'>
-                                    <button type="submit" class="">Validar</button>
-                                </div>
+                                <button class="col-4" type="submit" class="">Validar</button>
                             </div>
-                        </form>    
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
-    @endsection
-    
+@endsection
