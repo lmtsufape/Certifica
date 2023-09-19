5110d228bb0146fbe3d9af78ac1db6e9eb0fe325
Douglas Filipe <batista.douglasfs@gmail.com>
2023-09-19 17:38:26 -0300

    Correção nos seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,11 +23,9 @@ class UserSeeder extends Seeder
                                 'password' => Hash::make('password'), 'perfil_id' => '2',
                                 'unidade_administrativa_id' => '1', 'cpf' => '646.459.834-15', 'celular' => '87999887766',
                                 'instituicao' => 'UFAPE', 'siape' => '6543REDS']);
+
         User::factory()->create(['name' => 'Gestor Institucional', 'email' => 'gestorinst@admin.teste', 'email_verified_at' => now(),
                                 'password' => Hash::make('password'), 'perfil_id' => '3',
-                                'unidade_administrativa_id' => '1', 'cpf' => '785.255.717-17']);
-        User::factory()->create(['name' => 'PREC', 'email' => 'certificacao.prec@ufape.edu.br', 'email_verified_at' => now(),
-            'password' => Hash::make('prec123'), 'perfil_id' => '3',
-            'unidade_administrativa_id' => '2']);
+                                'unidade_administrativa_id' => '2', 'cpf' => '785.255.717-17']);
     }
 }
