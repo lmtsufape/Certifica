efec1f59db52cf2151f3a5e395e76003abbfd541
Douglas Filipe <batista.douglasfs@gmail.com>
2024-07-22 17:19:51 -0300

    fix: correção no menu dropdown 3.0

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -23,7 +23,7 @@
                            data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
                             <span class="font-weight-bolder">Olá, </span>{{ Auth::user()->name }}
                         </a>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('perfil.edit') }}">
                                 {{ __('Editar Perfil') }}
                             </a>
