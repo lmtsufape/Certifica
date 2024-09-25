da1fda3b259d267bb8347e82ac9fcd6f87bf241d
Douglas <batista.douglasfs@gmail.com>
2024-09-25 15:40:53 -0300

    fix: ajustes para o adm conseguir configurar os tipos de atividades

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -53,6 +53,13 @@
                         <span class="text-center">Relatórios</span>
                     </div>
                 </a>
+
+                <a class="link-opt" href="{{route('tipoatividade.index')}}">
+                    <div class="box-opt d-flex flex-column align-items-center justify-content-evenly">
+                        <img class="icon-opt" src="{{ '/images/home/tipo_atividade.svg' }}" alt="">
+                        <span class="text-center">Tipo de Atividades</span>
+                    </div>
+                </a>
             </div>
 
         </div>
