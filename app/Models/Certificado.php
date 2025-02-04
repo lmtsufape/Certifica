3b699d505093d011887acfc54928ed3307e6d507
Douglas Filipe <batista.douglasfs@gmail.com>
2025-02-04 17:20:05 -0300

    fix: correção na exibição das estatísticas dos relatórios v3

--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -48,9 +48,6 @@ class Certificado extends Model
     }
 
     public static function search_ano($certificados, $ano){
-        return $certificados->where(function($query) use ($ano) {
-                $query->whereYear('data_personalizada', $ano)
-                    ->orWhereNull('data_personalizada');
-                })->whereYear('created_at', $ano);
+        return $certificados->where('created_at.year', $ano);
     }
 }
