4b05c9edf94e26850c0db3adb41ebbdad8609a7d
Douglas Filipe <batista.douglasfs@gmail.com>
2025-02-07 16:40:56 -0300

    fix: remover limitação de apenas um nome único de atividades no sistema

--- app/Http/Requests/StoreTipoAtividade.php
+++ app/Http/Requests/StoreTipoAtividade.php
@@ -24,7 +24,7 @@ class StoreTipoAtividade extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string||min:5',
+            'name' => 'required|string|min:5',
         ];
     }
 
