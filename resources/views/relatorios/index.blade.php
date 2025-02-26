fb542d10ab53d00ba3a983a6e79f5a9ef29b99e0
Douglas Filipe <batista.douglasfs@gmail.com>
2025-02-26 14:26:58 -0300

    fix: correção no modal das estatísticas dos certificados

--- resources/views/relatorios/index.blade.php
+++ resources/views/relatorios/index.blade.php
@@ -85,51 +85,7 @@
         </div>
 
         <div class="list container">
-            @foreach($acoes as $acao)
-                <div class="row linha-table d-flex align-items-center justify-content-start">
-                    <div class="col-2 text-center titulo-span" title="{{$acao->titulo}}"><span>{{$acao->titulo}}</span></div>
-                    <div class="col-2 titulo-span text-center"><span class="spacing-col">{{$acao->tipo_natureza->natureza->descricao}}</span></div>
-                    <div class="col-2 text-center titulo-span" title="{{$acao->tipo_natureza->descricao}}"><span>{{$acao->tipo_natureza->descricao}}</span></div>
-                    <div class="col-2 text-center titulo-span" title="{{$acao->nome_atividades}}">
-                        <a href="{{route('relatorios.atividades', ['acao_id'=>$acao->id])}}" target="blank" ><img src="/images/atividades/participantes.svg" alt=""></a></div>
-                    <div class="col-1 text-center"><span>{{ $acao->total }}</span></div>
-                    <div class="col-1 text-center">
-                        <span> <a href="{{route('certificados.download', ['acao_id'=>$acao->id])}}" target="blank">
-                                    <img src="/images/acoes/listView/zipcertificados.svg" alt="Visualizar" title="Baixar Certificados">
-                                </a>
-                        </span>
-                    </div>
-                    <div class="col-2 text-center">
-                        <span>
-                            <a class="btn" data-bs-toggle="modal" data-bs-target="#modal-info{{ $acao->id }}">
-                                <img src="/images/acoes/listView/eye.svg" alt="" title="Estatísticas de Certificados"></a>
-                        </span>
-                    </div>
-                </div>
-
-                <div class="modal fade" id="modal-info{{ $acao->id }}">
-                    <div class="modal-dialog modal-dialog-centered" role="dialog">
-                        <div class="modal-content">
-                            <div class="modal-header" style="background: #972E3F; color: white;">
-                                <h5 class="modal-title"><b>Estatísticas de Certificados</b></h5>
-                            </div>
-                            <div class="modal-body">
-                                @if (count($acao->atividades))
-                                    <div class="row justify-content-center">
-                                        @foreach ($acao->atividades as $atividade)
-                                            <?php
-                                                $atividade->certificados = $atividade->certificados()->count();
-                                            ?>
-
-                                            <span><b>{{ $atividade->descricao }}:</b> {{ $atividade->certificados }} certificado(s)</span>
-                                        @endforeach
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            @include('relatorios.list')
         </div>
     </section>
 @endsection
