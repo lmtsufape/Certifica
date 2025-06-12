a0bb7f432e86e1a220e5d5ead5c272f3cdb320ac
Douglas Filipe <batista.douglasfs@gmail.com>
2025-06-11 21:33:24 -0300

    fix: correção no download dos certificados zipados

--- app/Jobs/ZipCertificados.php
+++ app/Jobs/ZipCertificados.php
@@ -26,6 +26,8 @@ use File;
 use Throwable;
 use Carbon\Carbon;
 use App\Mail\EnviarZipCertificados;
+use App\Models\Curso;
+use Illuminate\Support\Facades\File as FileFacade;
 
 class ZipCertificados implements ShouldQueue
 {
@@ -50,12 +52,12 @@ class ZipCertificados implements ShouldQueue
         if ($zip->open($zipname, ZipArchive::CREATE | ZipArchive::OVERWRITE) == true) {
             foreach ($this->acao->atividades as $atividade) {
                 foreach ($atividade->participantes as $participante) {
-                    $nomePDF = $caminho.DIRECTORY_SEPARATOR.'certificado - '.$participante->user->name.'.pdf';
+                    $nomePDF = $caminho.DIRECTORY_SEPARATOR.'Certificado '.$atividade->descricao.' - '.$participante->user->name.'.pdf';
                     array_push($certificados, $nomePDF);
 
                     $pdf = $this->montar_certificado($participante, $atividade, $nomePDF);
 
-                    $zip->addFile($nomePDF, 'certificado - '.$participante->user->name.'.pdf');
+                    $zip->addFile($nomePDF, 'Certificado '.$atividade->descricao.' - '.$participante->user->name.'.pdf');
 
                     $batchCount++;
 
@@ -85,32 +87,57 @@ class ZipCertificados implements ShouldQueue
     private function montar_certificado($participante, $atividade, $nomePDF)
     {
         Carbon::setLocale('pt_BR');
-        $tipo_natureza = TipoNatureza::findOrFail($this->acao->tipo_natureza_id);
-        $natureza = Natureza::findOrFail($tipo_natureza->natureza_id);
-
-        $data_inicio = Carbon::parse($atividade->data_inicio)->isoFormat('LL');
-        $data_fim = Carbon::parse($atividade->data_fim)->isoFormat('LL');
 
         $autorTrabalhoId = $participante->autor_trabalhos_id;
         $coautorTrabalhoId = $participante->coautor_trabalhos_id;
 
         $trabalho = Trabalho::whereIn('id', [$autorTrabalhoId, $coautorTrabalhoId])->first();
 
-        $info_extra_participante = $participante->info_externa_participante_id;
-        if($info_extra_participante){
-            $info_extra_participante = InfoExternaParticipante::findOrFail($info_extra_participante);
+        $acao = Acao::findOrFail($atividade->acao_id);
+
+        $tipo_natureza = TipoNatureza::findOrFail($acao->tipo_natureza_id);
+        $natureza = Natureza::findOrFail($tipo_natureza->natureza_id);
+
+        if($acao->data_personalizada) {
+            $data_atual = Carbon::parse($acao->data_personalizada)->isoFormat('LL');
+        }
+        else {
+            $data_atual = Carbon::parse(Carbon::now())->isoFormat('LL');
         }
 
-        $data_atual = Carbon::parse(Carbon::now())->isoFormat('LL');
-
-        $certificado = Certificado::where('cpf_participante', $participante->user->cpf)->where('atividade_id', $atividade->id)->first();
+        if($participante->user->cpf) {
+            $certificado = Certificado::where('cpf_participante', $participante->user->cpf)->where('atividade_id', $atividade->id)->first();
+        }
+        else {
+            $certificado = Certificado::where('cpf_participante', $participante->user->passaporte)->where('atividade_id', $atividade->id)->first();
+        }
 
         $modelo = CertificadoModelo::findOrFail($certificado->certificado_modelo_id);
 
-        $atividade->descricao = Str::lower($atividade->descricao);
 
-        $modelo->texto = CertificadoController::convert_text($modelo, $participante, $this->acao, $atividade, $natureza, $tipo_natureza,$trabalho,$info_extra_participante );
+        //$atividade->descricao = Str::lower($atividade->descricao);
 
+        if($atividade->data_inicio == $atividade->data_fim && $modelo->texto_um_dia != null) {
+            $modelo->texto = $modelo->texto_um_dia;
+        }
+
+        if(mb_strlen($modelo->texto) <= 380) {
+            $tamanho_fonte = 38;
+        }
+        else {
+            $tamanho_fonte = 38;
+            $excesso_caracteres = mb_strlen($modelo->texto) - 380;
+
+            if ($excesso_caracteres > 0) {
+                $reducoes_tamanho = ceil($excesso_caracteres / 65);
+                $tamanho_fonte -= $reducoes_tamanho * 2;
+            }
+
+            $tamanho_fonte = intval($tamanho_fonte);
+        }
+
+        $modelo->texto = $this->convert_text($modelo, $participante, $acao, $atividade, $natureza, $tipo_natureza, $trabalho);
+       
         $imagem = Storage::url($modelo->fundo);
 
         $verso = Storage::url($modelo->verso);
@@ -119,14 +146,58 @@ class ZipCertificados implements ShouldQueue
 
 
         $pdf = Pdf::loadView('certificado.gerar_certificado', compact('modelo', 'participante',
-                            'imagem', 'data_atual', 'certificado', 'qrcode', 'verso'));
+                            'imagem', 'data_atual', 'certificado', 'qrcode', 'verso', 'tamanho_fonte'));
 
 
         $pdf->set_option("dpi", 150)->setPaper('a4', 'landscape');
         $pdf->render();
         $output = $pdf->output();
 
-
         return file_put_contents($nomePDF, $output);
+    }
+
+    private static function convert_text($modelo, $participante, $acao, $atividade, $natureza, $tipo_natureza, $trabalho){
+        $data_inicio = Carbon::parse($atividade->data_inicio)->isoFormat('LL');
+        $data_fim = Carbon::parse($atividade->data_fim)->isoFormat('LL');
+
+        $pattern = '/\*[\wÀ-ú\%\.\,\_\-\(\)\#\@\!\'\ "]+\*/i';
+        $replace = '<b>$0</b>';
+
+        $modelo->texto = preg_replace($pattern, $replace, $modelo->texto);
+        $user = $participante->user;
+        $curso = json_decode($user->json_cursos_ids) ;
+
+        if($curso){
+            $curso =  (int) reset($curso);
+            $curso = Curso::find($curso);
+
+            if($curso){
+                $curso = $curso->nome;
+            }
+        }
+
+        if($atividade->descricao == "apresentação de trabalho")
+        {
+            if ($trabalho->nomesCoautoresComoTexto() == null) {
+                $modelo->texto = str_replace('tendo como coautores %coautores_trabalho%,', '', $modelo->texto);
+
+            }
+        }
+
+        if($trabalho){
+            $antes = array('%participante%', '%acao%', '%nome_atividade%', '%atividade%', '%data_inicio%', '%data_fim%', '%carga_horaria%', '%natureza%', '%tipo_natureza%', '*',
+                '%titulo_trabalho%', '%autores_trabalho%', '%coautores_trabalho%', '%curso%', '%titulo_atividade%');
+            $depois = array($participante->user->name, $acao->titulo, $participante->titulo, $atividade->descricao, $data_inicio, $data_fim, $participante->carga_horaria,
+                $natureza->descricao, $tipo_natureza->descricao, '', $trabalho->titulo, $trabalho->nomesAutoresComoTexto(), $trabalho->nomesCoautoresComoTexto(), $curso, $atividade->titulo);
+
+        } 
+        else{
+            $antes = array('%participante%', '%acao%', '%nome_atividade%', '%atividade%', '%data_inicio%', '%data_fim%', '%carga_horaria%', '%natureza%', '%tipo_natureza%', '*', '%curso%', '%titulo_atividade%');
+            $depois = array($participante->user->name, $acao->titulo, $participante->titulo, $atividade->descricao, $data_inicio, $data_fim,
+                $participante->carga_horaria, $natureza->descricao, $tipo_natureza->descricao, '', $curso, $atividade->titulo);
+
+        }
+
+        return str_replace($antes, $depois, $modelo->texto);
     }
 }
